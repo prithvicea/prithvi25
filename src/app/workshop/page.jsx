b2ca9a6eb3d52c7d7794b2fb8baaa6a8e479429f
ARUNKRISH11 <krish.tech.arun.11@gmail.com>
2025-02-26 00:04:16 +0530

    workshop page started

---
+++ src/app/workshop/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarWorkshop from './NavbarWorkshop'
+
+function Workshop() {
+  return (
+    <div>
+        <NavbarWorkshop/>
+    </div>
+  )
+}
+
+export default Workshop
